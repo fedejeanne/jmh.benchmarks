16bcd418821a634b91e6bb8fc88d09092acfc15b
fjeanne <fedejeanne@gmail.com>
2020-05-11 08:24:49 +0200

    Generate the random numbers for the BenchmarkDoAddMethods only once in order to make it a constant in the benchmarking.

--- src/main/java/org/fjeanne/jmh/benchmarks/defaultmethods/BenchmarkDoAddMethods.java
+++ src/main/java/org/fjeanne/jmh/benchmarks/defaultmethods/BenchmarkDoAddMethods.java
@@ -4,27 +4,30 @@ import org.openjdk.jmh.annotations.Benchmark;
 
 public class BenchmarkDoAddMethods {
 
+	private static final double A = Math.random();
+	private static final double B = Math.random();
+
 	@Benchmark
 	public void benchmarkDepth1Default() {
 		Interface child1 = new Children.Child1();
-		child1.doAddDefault(Math.random(), Math.random());
+		child1.doAddDefault(A, B);
 	}
 
 	@Benchmark
 	public void benchmarkDepth1Overridden() {
 		Interface child1 = new Children.Child1();
-		child1.doAddOverridden(Math.random(), Math.random());
+		child1.doAddOverridden(A, B);
 	}
 
 	@Benchmark
 	public void benchmarkDepth10Default() {
 		Interface child10 = new Children.Child10();
-		child10.doAddDefault(Math.random(), Math.random());
+		child10.doAddDefault(A, B);
 	}
 
 	@Benchmark
 	public void benchmarkDepth10Overridden() {
 		Interface child10 = new Children.Child10();
-		child10.doAddOverridden(Math.random(), Math.random());
+		child10.doAddOverridden(A, B);
 	}
 }
